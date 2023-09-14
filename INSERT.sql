d4b4f2493dcb7ec93cfb85e0472c56f3cbd1713d
Josue Romero <josueromram3@gmail.com>
2023-09-13 21:48:05 -0500

    Update files

--- INSERT.sql
+++ INSERT.sql
@@ -1,10 +1,3 @@
--- Estructura de inserción masiva para las tablas
-
-INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
-(1, 'App\\Models\\User', 1),
-(1, 'App\\Models\\User', 2);
-
-
 INSERT INTO `peliculas` (`id`, `nombre`, `descripcion`, `director`, `estudio`, `genero`, `precio`, `duracion`, `trailer`, `portada`, `exclusividad`, `foto`, `created_at`, `updated_at`) VALUES
 (1, 'Avatar: el camino del agua', 'Jake Sully y Ney\'tiri han formado una familia y hacen todo lo posible por permanecer juntos. Sin embargo, deben abandonar su hogar y explorar las regiones de Pandora cuando una antigua amenaza reaparece.', 'James Cameron', 'Walt Disney Studios Motion Pictures', 'Accion', 14000, '130', 'https://www.youtube.com/watch?v=u0hxjdWG84k', '', 'si', '1.jpg', '2023-09-10 07:22:52', '2023-09-10 23:00:50'),
 (2, 'Fuera de control', 'El horrendo día de Rachel, madre soltera, se torna una pesadilla infernal cuando toca la bocina a otro conductor. Lo que empieza como una discusión corriente por el tráfico se vuelve, en la cabeza de un psicópata, en una afrenta que merece venganza.', 'Derrick Borte', 'Ingenious Media; Burek Films', 'Accion', 20000, '91', 'https://www.youtube.com/embed/GoqLErZfoq4', '', 'si', '2.jpg', '2023-09-01 07:22:52', '2023-09-10 19:09:54'),
@@ -32,3 +25,6 @@ INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Josué Romero', 'admin@cinemajje.com', NULL, '$2y$10$aM0P/lwto5BczsJOfPdnSetj.TST9Q/bKPV.k815PXLUTKJcV6Qtu', NULL, '2023-09-10 08:17:20', '2023-09-10 08:17:20');
+
+INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
+(1, 'App\\Models\\User', 1),
